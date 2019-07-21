0cb7b1730c6584c462a17871979fb01f0305f399
Oliver Boamah <oliverboamah@yahoo.com>
2019-07-21 01:23:38 -1200

    update upload_file html template.

--- DataCollationApp/urls.py
+++ DataCollationApp/urls.py
@@ -19,7 +19,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('users/', views.users),
-    path('upload_file/', views.upload_file)
+    path('', views.index, name='index'),
+    path('users/', views.users, name='users'),
+    path('upload_file/', views.upload_file, name='upload_file'),
+    path('upload/', views.upload, name='upload')
 ]
