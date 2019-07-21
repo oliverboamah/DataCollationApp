3901f5d5c41b809bce0f0361f311d133a9d7a214
Oliver Boamah <oliverboamah@yahoo.com>
2019-07-21 02:45:07 -1200

    install mysqlclient

--- DataCollationApp/views.py
+++ DataCollationApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from openpyxl import load_workbook
 
 
 # Create your views here.
@@ -22,7 +23,22 @@ def upload(request):
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+
+        work_book = load_workbook('media/fos.xlsx')
+
+        sheet = work_book.get_sheet_by_name(work_book.sheetnames[1])
+        all_data = sheet[2:sheet.max_row]
+        cell = sheet['A2']
+        multi_data = []
+        for i in range(sheet.max_row):
+            multi_data.append([sheet['A' + str(i+2)].value, sheet['B' + str(i+2)].value,
+                               sheet['C' + str(i+2)].value,
+                               sheet['D' + str(i+2)].value, sheet['E' + str(i+2)].value
+                               ])
+
+
+
         return render(request, 'upload_file.html', {
-            'uploaded_file_url': uploaded_file_url
+            'uploaded_file_url': multi_data
         })
     return render(request, 'upload_file.html')
