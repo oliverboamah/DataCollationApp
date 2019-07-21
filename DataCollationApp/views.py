63f49ce57d83a6cb7399b401b4a69abea57774f5
Oliver Boamah <oliverboamah@yahoo.com>
2019-07-20 23:22:17 -1200

    add homepage, users html template.

---
+++ DataCollationApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def users(request):
+    return render(request, 'users.html', {})
